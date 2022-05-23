0605b11ae8847e0648c22120b05655695b6e5e74
Rachid Essantouh <rachid.essantouh@gmail.com>
2022-05-23 21:24:07 +0200

    Exercise 4 done

--- Jest exercises/src/__tests__/04-exercise.test.js
+++ Jest exercises/src/__tests__/04-exercise.test.js
@@ -32,6 +32,11 @@ describe("04-exercises", () => {
      */
 
     // Write the assertions
+
+    const obj = createUserObject("Alex", "Spence", "alex@mail.com")
+    expect((obj).firstName).toBeTruthy();
+    expect((obj).lastName).toBeTruthy();
+    expect((obj).email).toBeTruthy();
   });
 
   test("createCorporateEmailAddress appends the corporate email domain", () => {
@@ -45,6 +50,8 @@ describe("04-exercises", () => {
      */
 
     // Write the assertion
+
+    expect(createCorporateEmailAddress("dani")).toMatch("dani@company.com")
   });
 
   test("generateRandomNumberFrom1to10 returns a number between 1 and 10", () => {
@@ -56,5 +63,7 @@ describe("04-exercises", () => {
      */
 
     // Write the assertions
+    expect(generateRandomNumberFrom1to10()).toBeGreaterThan(0)
+    expect(generateRandomNumberFrom1to10()).toBeLessThanOrEqual(10)
   });
 });
