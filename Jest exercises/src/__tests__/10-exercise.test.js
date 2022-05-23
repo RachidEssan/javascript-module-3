3cad29a831f2d3673fa883b67f3a09151594e023
Rachid Essantouh <rachid.essantouh@gmail.com>
2022-05-23 21:24:59 +0200

    Exercise 10 done

--- Jest exercises/src/__tests__/10-exercise.test.js
+++ Jest exercises/src/__tests__/10-exercise.test.js
@@ -7,10 +7,14 @@ describe("10-exercises", () => {
   beforeAll(() => {
     // 1. set the default inner html of the document body
     //    to have a div with a class of .wrapper
+
+    const wrapper = document.createElement("div")
+    wrapper.classList.add("wrapper")
+    document.body.appendChild(wrapper)
   });
 
   test("adds a button to the dom", () => {
-    // You should make 1 assertion in this test, see bellow
+    // You should make 1 assertion in this test, see below
     expect.assertions(1);
 
     const btnText = "Save";
@@ -18,11 +22,18 @@ describe("10-exercises", () => {
     // 2. execute the addButton function and pass it as an argument
     //    the `btnText` variable
 
+    addButton(btnText)
+
     // 3. use document.querySelector to get the new button
     //    the `addButton` creates a button with a class of .btn
     //    that you can use in document.querySelector
 
+    const newButton = document.querySelector(".btn")
+
     // 4. make an assertion that the buttons textContent is
     //    the same as the `btnText` variable
+
+    expect(newButton.textContent).toEqual(btnText)
+
   });
 });
